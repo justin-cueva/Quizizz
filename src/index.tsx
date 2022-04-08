b8de724ea3d0b14c3aaa3e62a4f18e8d944e1cfd
justincueva02 <justincueva02@gmail.com>
2022-04-08 13:01:16 -0700

    setup thunk and redux dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./components/App";
 import reducers from "./reducers";
@@ -10,7 +12,11 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
 root.render(
   <Provider store={store}>
     <App />
