058900dcee19ff9c6d0d2126c31be6667e1fcbd0
justincueva02 <justincueva02@gmail.com>
2022-04-08 12:56:04 -0700

    setup redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import "./styles/index.css";
 import App from "./components/App";
+import reducers from "./reducers";
+import "./styles/index.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+
+const store = createStore(reducers);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
