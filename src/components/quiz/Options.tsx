3d43d11942f186fb3ee1d47cf1544e90e565024b
justincueva02 <justincueva02@gmail.com>
2022-04-09 16:10:12 -0700

    pausing before we show the next question

--- src/components/quiz/Options.tsx
+++ src/components/quiz/Options.tsx
@@ -1,17 +1,61 @@
-// PROPS NEEDED
-// currentQuestion
-// addToScore
-// setQuestionNumber
-// numberOfQuestions
-// questionNumber
-// useNavigate from react router
-import "../../styles/quiz/CurrentQuestion.css";
-
-// PROPS NEEDED IN CURRENT QUESTION. TSX
+import { useState } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
 import { Question } from "../../types";
 import { addToScore } from "../../actions/index";
+import "../../styles/quiz/CurrentQuestion.css";
+
+const Options = ({
+  quizQuestions,
+  questionNumber,
+  setQuestionNumber,
+  addToScore,
+}: Props) => {
+  const [isShowingAnswer, setIsShowingAnswer] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const numberOfQuestions = quizQuestions.length;
+  const currentQuestion = quizQuestions[questionNumber - 1];
+
+  const clickHandler = (option: any) => {
+    console.log("STARTED timer");
+    setIsShowingAnswer(true);
+    setTimeout(() => {
+      console.log("ENDED timer");
+      // when we get the question correct
+      if (currentQuestion.correct_answer === option) {
+        addToScore();
+      }
+      // when we finished the quiz
+      if (numberOfQuestions === questionNumber) {
+        setQuestionNumber(1);
+        navigate("/summary");
+        // going to next question
+      } else {
+        setQuestionNumber((prev) => prev + 1);
+      }
+      setIsShowingAnswer(false);
+    }, 5000);
+  };
+
+  return (
+    <div className="choices">
+      {currentQuestion?.options?.map((option, index) => {
+        return (
+          <div
+            key={index + 1}
+            className={`choice choice--${index + 1}`}
+            onClick={() => {
+              isShowingAnswer ? console.log("nothing") : clickHandler(option);
+            }}
+          >
+            {option}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 interface RootState {
   quizQuestions: Question[];
@@ -27,43 +71,6 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 type Props = PropsFromRedux & {
   questionNumber: number;
   setQuestionNumber: React.Dispatch<React.SetStateAction<number>>;
-};
-
-const Options = ({
-  quizQuestions,
-  questionNumber,
-  setQuestionNumber,
-  addToScore,
-}: Props) => {
-  const navigate = useNavigate();
-  const numberOfQuestions = quizQuestions.length;
-  const currentQuestion = quizQuestions[questionNumber - 1];
-
-  return (
-    <div className="choices">
-      {currentQuestion?.options?.map((option, index) => {
-        return (
-          <div
-            key={index + 1}
-            className={`choice choice--${index + 1}`}
-            onClick={() => {
-              if (currentQuestion.correct_answer === option) {
-                addToScore();
-              }
-              if (numberOfQuestions === questionNumber) {
-                setQuestionNumber(1);
-                navigate("/summary");
-              } else {
-                setQuestionNumber((prev) => prev + 1);
-              }
-            }}
-          >
-            {option}
-          </div>
-        );
-      })}
-    </div>
-  );
 };
 
 export default connector(Options);
