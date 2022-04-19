96fa2dd7c02789615cd18c81b624a5b71e7d2134
justincueva02 <justincueva02@gmail.com>
2022-04-19 09:55:22 -0700

    styled the pause modal

--- src/components/quiz/PauseModal.tsx
+++ src/components/quiz/PauseModal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { FaRunning } from "react-icons/fa";
 import "../../styles/quiz/Modal.css";
 
 type Props = {
@@ -15,8 +15,26 @@ const PauseModal = ({ element, closeModal }: Props) => {
     <React.Fragment>
       <div className="overlay" onClick={() => closeModal()} />
       <div className="modal">
-        <button onClick={() => closeModal()}>close</button>
-        PauseModal
+        <div className="container--relative modal__container--flex">
+          <div className="modal__progress-bar">
+            <div className="modal__progress--wrong">
+              <div className="modal__progress--correct" />
+            </div>
+            <span className="modal__icon--running">
+              <FaRunning />
+            </span>
+          </div>
+          <div className="modal__remaining-questions">
+            2 Questions Remaining
+          </div>
+          <button
+            type="button"
+            onClick={() => closeModal()}
+            className="modal__resume-button"
+          >
+            Resume
+          </button>
+        </div>
       </div>
     </React.Fragment>,
     element
