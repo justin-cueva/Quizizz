c9ba7f2cedca1ecb5105da164b015a2d84a9c34c
justincueva02 <justincueva02@gmail.com>
2022-04-23 09:56:09 -0700

    started my build reducer and actions

---
+++ src/actions/myBuildActions.ts
@@ -0,0 +1,42 @@
+import { Question } from "../types";
+
+export const addQuestion = (question: Question) => {
+  return {
+    type: "ADD_QUESTION_TO_MY_BUILD",
+    payload: question,
+  };
+};
+
+export const removeQuestion = (question: Question) => {
+  return {
+    type: "REMOVE_QUESTION_FROM_MY_BUILD",
+    payload: question,
+  };
+};
+
+export const nameQuiz = (name: String) => {
+  return {
+    type: "NAME_MY_BUILD",
+    payload: name,
+  };
+};
+
+export const saveBuild = () => async (dispatch: any, getState: any) => {
+  try {
+    const usersId = getState().account.userId;
+    const nameOfBuild = getState().myBuild.name;
+    const questions = getState().myBuild.questions;
+
+    await fetch(
+      `https://quizizz-32675-default-rtdb.firebaseio.com/${usersId}/myBuilds/${nameOfBuild}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(questions),
+      }
+    );
+
+    dispatch({ type: "SAVED_BUILD" });
+  } catch (err) {
+    console.error(err);
+  }
+};
